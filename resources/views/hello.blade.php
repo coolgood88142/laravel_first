b64fd4bda8df7e09fcc3469e0e6a27bb7fffb0df
Kai <coolgood88142@gmail.com>
2020-09-15 05:10:17 +0800

    :rocket: larvel+vue實做

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <div id="app">
+            @{{ message }}
+        </div>
+        <script src="{{mix('js/app.js')}}"></script>
+        <script src="{{mix('js/hello.js')}}"></script>
+    </body>
+</html>
